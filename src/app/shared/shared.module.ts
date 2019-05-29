1778ebdebb83b311965540553a5daff5887c8f1f
Felipe Teles <fliptm95@gmail.com>
2019-05-29 06:31:28 -0300

    improving character list component

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule
+  ],
+  declarations: [
+  ],
+  exports: [
+    // shared modules
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule
+  ]
+})
+export class SharedModule { }
